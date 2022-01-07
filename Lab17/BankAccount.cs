9c2a54baf55ab73818651f0bb4a250d718306d61
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-01-07 12:48:01 +0300

    Добавьте файлы проекта.

---
+++ Lab17/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class BankAccount<T>
+    {
+        public T Numer;
+        public double Summa;
+        public string Name;
+
+        public void Enter()
+        {
+            Console.Write("Введите номер счёта - ");
+            Numer =(T) Convert.ChangeType(Console.ReadLine(),typeof(T));
+            Console.Write("Введите баланс - ");
+            Summa = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ФИО - ");
+            Name = Console.ReadLine();
+        }
+
+        public void Print()
+        {
+            using (StreamWriter sw = new StreamWriter("BankAccount.txt"))
+            {
+                sw.WriteLine($"{Numer} - {Summa} - {Name}");
+            }
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankaccount1 = new BankAccount<int>();
+            bankaccount1.Enter();
+            bankaccount1.Print();
+            Console.ReadKey();
+
+            BankAccount<string> bankaccount2 = new BankAccount<string>();
+            bankaccount2.Enter();
+            bankaccount2.Print();
+            Console.ReadKey();
+        }
+    }
+}
